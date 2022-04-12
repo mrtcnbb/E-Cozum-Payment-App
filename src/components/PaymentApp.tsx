43b5713950aae6a8c2ae17a43eb7e7e17b931487
Muratcan Baba <muratcanbaba1@gmail.com>
2022-04-12 18:05:18 +0300

    Add: Auth, Selection, Payment, Info pages

--- src/components/PaymentApp.tsx
+++ src/components/PaymentApp.tsx
@@ -1,8 +1,21 @@
-import { Box } from '@chakra-ui/react';
-import React, { FC } from 'react';
+import { FC } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Auth from '../pages/auth/Auth';
+import Info from '../pages/info/Info';
+import Payment from '../pages/payment/Payment';
+import Selection from '../pages/selection/Selection';
 
 const PaymentApp: FC = () => {
-  return <Box height="100vh">PaymentApp</Box>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/selection" element={<Selection />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default PaymentApp;
---
+++ src/pages/selection/Selection.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Selection() {
+  return <div>Selection</div>;
+}
+
+export default Selection;
