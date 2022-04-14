b890e6a1478a25558918a6a3cdf03b73db62d4d2
Muratcan Baba <muratcanbaba1@gmail.com>
2022-04-14 14:30:16 +0300

    Edit: Info page

--- src/pages/info/Info.tsx
+++ src/pages/info/Info.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { Box, Icon, Text } from '@chakra-ui/react';
+import { FC } from 'react';
+import Header from '../../components/Header';
+import { BiCheckCircle } from 'react-icons/bi';
 
-function Info() {
-  return <div>Info</div>;
-}
+const Info: FC = () => {
+  return (
+    <Box display="flex" flexDirection="column" height="100vh" bg="gray.200">
+      <Header />
+      <Box
+        display="flex"
+        flexDirection="column"
+        p="30px"
+        rounded="lg"
+        height="200px"
+        width="350px"
+        bg="white"
+        shadow="md"
+        my="auto"
+        mx="auto"
+        justifyContent="start"
+        alignItems="center"
+      >
+        <Icon color="green" boxSize="24" as={BiCheckCircle} />
+        <Text fontSize="xl" fontWeight="extrabold">
+          Başarıyla Tamamlandı!
+        </Text>
+      </Box>
+    </Box>
+  );
+};
 
 export default Info;
